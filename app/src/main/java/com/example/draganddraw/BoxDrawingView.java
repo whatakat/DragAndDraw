fdff748690acfadb134cb425b68bba7c4307f636
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-25 18:53:44 +0300

    Add class BoxDrawingView

---
+++ app/src/main/java/com/example/draganddraw/BoxDrawingView.java
@@ -0,0 +1,14 @@
+package com.example.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+    public BoxDrawingView(Context context){
+        this(context, null);
+    }
+    public BoxDrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+}
