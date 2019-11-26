360d4595075700aae6acee65f96911fe258207bf
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-26 20:54:25 +0300

    implementation BoxDrawingView

--- app/src/main/java/com/example/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/draganddraw/BoxDrawingView.java
@@ -1,14 +1,41 @@
 package com.example.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
     public BoxDrawingView(Context context){
         this(context, null);
     }
     public BoxDrawingView(Context context, AttributeSet attrs){
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+                case MotionEvent.ACTION_UP:
+                    action = "ACTION_UP";
+                    break;
+                    case MotionEvent.ACTION_CANCEL:
+                        action = "ACTION_CANCEL";
+                        break;
+        }
+        Log.i(TAG, action+" at x="+current.x+
+                ", y="+current.y);
+        return true;
     }
 }
