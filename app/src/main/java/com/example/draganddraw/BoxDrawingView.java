f1d72b042ab6464c55517e2572a74ecbccd4c703
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-26 21:22:11 +0300

    the methods life cycle added

--- app/src/main/java/com/example/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/draganddraw/BoxDrawingView.java
@@ -7,8 +7,13 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
     public BoxDrawingView(Context context){
         this(context, null);
     }
@@ -23,15 +28,23 @@ public class BoxDrawingView extends View {
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox!=null){
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
                 case MotionEvent.ACTION_UP:
                     action = "ACTION_UP";
+                    mCurrentBox = null;
                     break;
                     case MotionEvent.ACTION_CANCEL:
                         action = "ACTION_CANCEL";
+                        mCurrentBox = null;
                         break;
         }
         Log.i(TAG, action+" at x="+current.x+
